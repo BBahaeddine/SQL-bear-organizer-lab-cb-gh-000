630400a68ce69190d6bedeffa3cd31af92be2163
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 10:31:28 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,8 @@
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', NUL, NUL, NUL, NUL, NUL);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Rowdy', NUL, NUL, NUL, NUL, NUL);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Tabitha', NUL, NUL, NUL, NUL, NUL);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Sergeant Brown', NUL, NUL, NUL, NUL, NUL);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Melissa', NUL, NUL, NUL, NUL, NUL);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Grinch', NUL, NUL, NUL, NUL, NUL);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Windy', NUL, NUL, NUL, NUL, NUL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Rowdy', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Tabitha', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Sergeant Brown', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Melissa', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Grinch', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Windy', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 13, "F", "blue", NULL, NULL);
