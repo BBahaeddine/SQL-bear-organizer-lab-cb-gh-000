40737d6ed884602a7650193b39e62f58da10ea21
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 10:29:07 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Mr. Chocolate', NUL, NUL, NUL, NUL, NUL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Rowdy', NUL, NUL, NUL, NUL, NUL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Tabitha', NUL, NUL, NUL, NUL, NUL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Sergeant Brown', NUL, NUL, NUL, NUL, NUL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Melissa', NUL, NUL, NUL, NUL, NUL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Grinch', NUL, NUL, NUL, NUL, NUL);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES('Windy', NUL, NUL, NUL, NUL, NUL);
